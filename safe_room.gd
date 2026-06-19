58cd65614b6527463a0f115ccacc55524de65983
vanam-hansika <vanamhansika777@gmail.com>
2026-06-19 16:11:31 +0530

    Added third safe room, changed door to white, and fixed door auto-close logic

--- safe_room.gd
+++ safe_room.gd
@@ -6,6 +6,9 @@ extends Node3D
 var door_open = false
 var tween: Tween
 
+var player_in_door_trigger = false
+var player_in_safe_zone = false
+
 func _ready():
 	$DoorTrigger.body_entered.connect(_on_door_trigger_entered)
 	$DoorTrigger.body_exited.connect(_on_door_trigger_exited)
@@ -14,10 +17,39 @@ func _ready():
 
 func _on_door_trigger_entered(body):
 	if body.is_in_group("player"):
-		open_door()
+		player_in_door_trigger = true
+		update_door_state()
 
 func _on_door_trigger_exited(body):
 	if body.is_in_group("player"):
+		player_in_door_trigger = false
+		update_door_state()
+
+func _on_safe_zone_entered(body):
+	if body.is_in_group("player"):
+		player_in_safe_zone = true
+		body.is_safe = true
+		if audio_manager and audio_manager.has_method("set_safe_mode"):
+			audio_manager.set_safe_mode(true)
+		update_door_state()
+
+func _on_safe_zone_exited(body):
+	if body.is_in_group("player"):
+		player_in_safe_zone = false
+		body.is_safe = false
+		if audio_manager and audio_manager.has_method("set_safe_mode"):
+			audio_manager.set_safe_mode(false)
+		update_door_state()
+
+func update_door_state():
+	if player_in_safe_zone:
+		# Player is inside the safe room, close the door!
+		close_door()
+	elif player_in_door_trigger:
+		# Player is approaching or leaving (outside the safe zone but in trigger), open the door!
+		open_door()
+	else:
+		# Player has left the area entirely, close the door!
 		close_door()
 
 func open_door():
@@ -38,15 +70,3 @@ func close_door():
 	if tween: tween.kill()
 	tween = create_tween()
 	tween.tween_property(door_body, "position:x", 0.0, 0.8).set_trans(Tween.TRANS_SINE)
-
-func _on_safe_zone_entered(body):
-	if body.is_in_group("player"):
-		body.is_safe = true
-		if audio_manager and audio_manager.has_method("set_safe_mode"):
-			audio_manager.set_safe_mode(true)
-
-func _on_safe_zone_exited(body):
-	if body.is_in_group("player"):
-		body.is_safe = false
-		if audio_manager and audio_manager.has_method("set_safe_mode"):
-			audio_manager.set_safe_mode(false)
